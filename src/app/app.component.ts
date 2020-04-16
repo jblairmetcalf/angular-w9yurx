5e5599e66a9f45b2f2a8365040eee2d509931a97
jblairmetcalf <me@jblairmetcalf.com>
2020-04-16 10:01:57 -0700

    4 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,17 @@
 import { Component } from '@angular/core';
-// AfterViewInit
 
 import {PostMessageAbstract} from './post-message.abstract';
-
-// import { AngularFireAuth } from 'angularfire2/auth';
-// import { AngularFireAuth } from '@angular/fire/auth';
-// import { auth } from 'firebase/app';
-// import { AuthService } from './auth.service';
-
-// import { environment } from './environment';
-
-// import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  // providers: [AngularFireAuth],
 })
 export class AppComponent extends PostMessageAbstract {
-  // implements AfterViewInit
-  // public user: Observable<firebase.User>;
   public scrollIntoView = true;
   public behavior = 'smooth';
   public block = 'center';
   public inline = 'start';
-
-  // constructor(public auth: AngularFireAuth) {
-  //   super();
-
-  //   this.user = firebaseAuth.authState;
-  // }
-
-  // constructor(public auth: AuthService) {
-  //   super();
-  // }
-
-  // ngAfterViewInit() {
-    // this.scrollIntoView = true;
-    // this.behavior = 'smooth';
-    // this.block = 'center';
-    // this.inline = 'start';
-
-    // this.authService.signIn();
-  // }
-
-  public login() {
-    // this.auth.signInWithPopup(new auth.GoogleAuthProvider());
-  }
-
-  public logout() {
-    // this.auth.signOut();
-  }
 
   public onClick(event) {
     // console.log('onClick()', event);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 
-// import { AngularFireModule } from 'angularfire2';
-// import { environment } from './environment';
-// import { AngularFireModule } from '@angular/fire';
-// import { AngularFireAuth } from '@angular/fire/auth';
-// import { AngularFireAuthModule } from '@angular/fire/auth';
-
-// import { AngularFireModule } from 'angularfire2';
-// import { AngularFireAuthModule } from 'angularfire2/auth';
-
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    MaterialModule,
-    // AngularFireModule.initializeApp(environment.firebase),
-    // AngularFireAuthModule
-  ], //, AngularFireAuthModule]
+  imports: [BrowserModule, FormsModule, MaterialModule],
   declarations: [AppComponent],
-  bootstrap: [AppComponent],
-  // providers: [AngularFireAuth]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
